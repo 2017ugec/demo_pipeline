44c7979eb06cb1e7d7f4c25bb64885be0dc4eec1
Rajat Kashyap <rajatk@sigmoidanalytics.com>
2023-05-09 19:51:31 +0530

    First

---
+++ myscript.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Sample pandas code
+data = {'Name': ['John', 'Jane', 'Mike'],
+        'Age': [25, 30, 35]}
+df = pd.DataFrame(data)
+print(df)
+
+## jenkins admin password :: d433cf8f2de140deaa98197aa38e1df7
+## code to get this password :: "docker exec my-jenkins cat /var/jenkins_home/secrets/initialAdminPassword"
+## starting jenkins :: "docker run -d -p 8080:8080 -p 50000:50000 --name jenkins jenkins/jenkins"
+
+## Github token :: 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
